df15010bf5baa345e5bfae3a1e0fa765160809ed
임진성 <imjinsung0319@gmail.com>
2022-08-07 11:23:03 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a,i)=>{
+    a.src = 'https://ilyo.co.kr/contents/article/images/2022/0715/1657849883857321.jpg'
+})
+
+},500)
